13b2980c4d3957ab62e71daa12cfc6a99e81d471
Hisyam Maulana <hisyammaulana100@gmail.com>
2020-04-11 15:15:53 +0700

    github finder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/layouts/Navbar'
+import Users from './components/users/Users'
+import Search from './components/users/Search'
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  searchUsers = async text => {
+    this.setState({loading: true})
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+                                 &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    
+    this.setState({users: res.data.items, loading: false})    
+  }
+  clearUsers = () => this.setState({ users: [], loading: false })
+  render() {
+    return (
+      <div className="App" >
+        <Navbar/>
+        <div className="container" >
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true : false} />
+          <Users loading={this.state.loading} users={this.state.users}/>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
